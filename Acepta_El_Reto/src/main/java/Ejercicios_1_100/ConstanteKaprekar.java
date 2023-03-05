eb44048ba13bb8be2d80daddf82a396c498dd7ba
David Morales <edavid.morales@udea.edu.co>
2023-03-04 20:30:57 -0500

    se agrega la primera parte del primer ejercicio

--- Acepta_El_Reto/src/main/java/Ejercicios_1_100/ConstanteKaprekar.java
+++ Acepta_El_Reto/src/main/java/Ejercicios_1_100/ConstanteKaprekar.java
@@ -8,7 +8,7 @@ public class ConstanteKaprekar {
 
     public static void main(String[] args) {
 
-        int numero = 0;
+        int numero;
         int cantidadDigitos = 1;
         int numDigitos = 10;
         int numeroRecibe;
@@ -29,8 +29,6 @@ public class ConstanteKaprekar {
         int [] myArray = new int [cantidadDigitos];
         int acum=numeroRecibe;
 
-        //   Arrays.sort(myArray);
-       // int valorFinal = myArray[myArray.length - 1];
         for(int i=0; i<cantidadDigitos; i++){
              myArray[cantidadDigitos - i - 1] = acum%10;
              acum = acum/10;
@@ -47,12 +45,45 @@ public class ConstanteKaprekar {
                 }
 
 
-        int sum = 0;
+        int numdesc = 0;
+
         for(int i = 0; i<cantidadDigitos; i++)
-            sum = sum + myArray[i]*(int)Math.pow(10,cantidadDigitos-1-i);
-       // return sum;
-        System.out.println("EL numero es: " + sum);
+            numdesc = numdesc + myArray[i]*(int)Math.pow(10,cantidadDigitos-1-i);
+
+        System.out.println("EL numero descentente es: " + numdesc);
+
+        do{
+            numero = numeroRecibe/numDigitos;
+            if(numero>0){
+                cantidadDigitos++;
+                numDigitos = numDigitos*10;
+            }
+        }while(numero!=0);
+
+        int [] myArrayasc = new int [cantidadDigitos];
+        int acumasc=numeroRecibe;
+
+        for(int i=0; i<cantidadDigitos; i++){
+            myArray[cantidadDigitos - i - 1] = acum%10;
+            acum = acum/10;
+
+        }
+        acum = 0;
+
+        for(int i=0; i<myArray.length-1;i++)
+            for(int x=0; x<myArray.length-i-1; x++)
+                if(myArray[x+1]>myArray[x]){
+                    acum = myArray[x];
+                    myArray[x]=myArray[x+1];
+                    myArray[x+1]=acum;
+                }
+
+
+        int numdesc = 0;
+
+        for(int i = 0; i<cantidadDigitos; i++)
+            numdesc = numdesc + myArray[i]*(int)Math.pow(10,cantidadDigitos-1-i);
+        System.out.println("EL numero descentente es: " + numdesc);
+      }
     }
-
-
 }
