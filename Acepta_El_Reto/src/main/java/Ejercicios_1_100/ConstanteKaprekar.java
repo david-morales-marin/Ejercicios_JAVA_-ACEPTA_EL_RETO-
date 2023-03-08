c7369bfc5c5e230591b55d6c070b8b6eeed5c7ce
DavidMorales <edavid.morales@udea.edu.co>
2023-03-08 12:26:58 -0500

    se agrega el metodo ascendente

--- Acepta_El_Reto/src/main/java/Ejercicios_1_100/ConstanteKaprekar.java
+++ Acepta_El_Reto/src/main/java/Ejercicios_1_100/ConstanteKaprekar.java
@@ -14,54 +14,59 @@ public class ConstanteKaprekar {
         int cantidadDigitos = 1;
         int numDigitos = 10;
         int numeroRecibe;
-
+        int valorFinal;
         Scanner numeroKaprekar = new Scanner(System.in);
 
         System.out.println("Ingrese un numero de 4 digitos: ");
 
         numeroRecibe = numeroKaprekar.nextInt();
-        do {
-            numero = numeroRecibe / numDigitos;
-            if (numero > 0) {
-                cantidadDigitos++;
-                numDigitos = numDigitos * 10;
-            }
-        } while (numero != 0);
-
-        int[] myArray = new int[cantidadDigitos];
-        int acum = numeroRecibe;
-
-        for (int i = 0; i < cantidadDigitos; i++) {
-            myArray[cantidadDigitos - i - 1] = acum % 10;
-            acum = acum / 10;
-
-        }
-        acum = 0;
-
-        for (int i = 0; i < myArray.length - 1; i++)
-            for (int x = 0; x < myArray.length - i - 1; x++)
-                if (myArray[x + 1] > myArray[x]) {
-                    acum = myArray[x];
-                    myArray[x] = myArray[x + 1];
-                    myArray[x + 1] = acum;
+       // if(  valorFinal = 6174){
+         //   System.out.println();
+       // }
+            do {
+                numero = numeroRecibe / numDigitos;
+                if (numero > 0) {
+                    cantidadDigitos++;
+                    numDigitos = numDigitos * 10;
                 }
+            } while (numero != 0);
+
+            int[] myArray = new int[cantidadDigitos];
+            int acum = numeroRecibe;
+
+            for (int i = 0; i < cantidadDigitos; i++) {
+                myArray[cantidadDigitos - i - 1] = acum % 10;
+                acum = acum / 10;
+
+            }
+            acum = 0;
+
+            for (int i = 0; i < myArray.length - 1; i++)
+                for (int x = 0; x < myArray.length - i - 1; x++)
+                    if (myArray[x + 1] > myArray[x]) {
+                        acum = myArray[x];
+                        myArray[x] = myArray[x + 1];
+                        myArray[x + 1] = acum;
+                    }
 
 
-        int numdesc = 0;
+            int numdesc = 0;
 
-        for (int i = 0; i < cantidadDigitos; i++)
-            numdesc = numdesc + myArray[i] * (int) Math.pow(10, cantidadDigitos - 1 - i);
+            for (int i = 0; i < cantidadDigitos; i++)
+                numdesc = numdesc + myArray[i] * (int) Math.pow(10, cantidadDigitos - 1 - i);
 
-        int numasc = 0;
-        Arrays.sort(myArray);
-         for(int i= 0; i < myArray.length; i++)
-            numasc =   myArray[i];
-          numasc =  System.out.print( myArray[i]);
-        //  System.out.println();
+            int numasc = 0;
+            Arrays.sort(myArray);
+            for (int i = 0; i < myArray.length; i++)
+                numasc = numasc + myArray[i] * (int) Math.pow(10, cantidadDigitos - 1 - i);
 
-        System.out.println("EL número descentente es: " + numdesc);
-        System.out.println("EL número ascendente es: " + numasc);
+            //   System.out.println("EL número descentente es: " + numdesc);
+            //  System.out.println("EL número ascendente es: " + numasc);
 
-    }
+                 valorFinal = numdesc - numasc;
+              //   valorFinal++;
+             System.out.println( numdesc + " - " + numasc + " = " + valorFinal);
 
+
+      }
     }
